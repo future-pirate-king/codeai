b3a3c94f8efbd00930be8b6808738ffecea14bd1
Sam David <samd948@gmail.com>
2019-01-12 09:42:11 +0530

    tslint for linting

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
